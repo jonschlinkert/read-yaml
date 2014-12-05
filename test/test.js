3cec37dd6f80c4aa19650d0ce1336a450407c18b
jonschlinkert <dev@sellside.com>
2014-12-05 07:23:38 -0500

    use should instead of chai

--- test/test.js
+++ test/test.js
@@ -7,54 +7,43 @@
 
 'use strict';
 
-var expect = require('chai').expect;
+var assert = require('assert');
+var should = require('should');
 var path = require('path');
-var readYaml = require('../');
 var YAML = require('js-yaml');
+var readYaml = require('..');
 
-var testYamlPath = 'test/fixture.yaml';
-var testYamlContents = {
-  foo: {
-    bar: 'baz',
-    qux: true
-  }
-};
 
 describe('readYaml', function () {
   it('should read the yaml file asynchronously.', function (done) {
-    var expected = testYamlContents;
-    readYaml(testYamlPath, 'utf8', function (err, actual) {
-      expect(err).to.be.null;
-      expect(actual).to.eql(expected);
+    readYaml('test/fixture.yaml', 'utf8', function (err, actual) {
+      assert.equal(err == null, true);
+      actual.should.eql({foo: {bar: 'baz', qux: true }});
       done();
     });
   });
   it('should support options.', function (done) {
-    readYaml(testYamlPath, {schema: YAML.FAILSAFE_SCHEMA}, function (err, actual) {
-      var expected = {
-        foo: {
-          bar: 'baz',
-          qux: 'true'
-        }
-      };
-      expect(err).to.be.null;
-      expect(actual).to.eql(expected);
+    readYaml('test/fixture.yaml', {schema: YAML.FAILSAFE_SCHEMA}, function (err, actual) {
+      assert.equal(err == null, true);
+      actual.should.eql({foo: {bar: 'baz', qux: 'true'}});
       done();
     });
   });
   it('should fail when it cannot parse the file as YAML.', function (done) {
     readYaml('index.js', {schema: YAML.FAILSAFE_SCHEMA}, function (err) {
-      expect(err).to.be.instanceof(YAML.YAMLException);
-      expect(err).to.have.property('message').to.contain('index.js');
-      expect(arguments).to.have.length(1);
+      err.should.be.an.instanceof(YAML.YAMLException);
+      err.should.have.property('message');
+      err.message.should.containEql('index.js');
+      arguments.should.have.length(1);
       done();
     });
   });
   it('should fail when it cannot read the file.', function (done) {
     readYaml('__foo__', function (err) {
-      expect(err).to.be.instanceof(Error);
-      expect(err).to.have.property('message').to.contain('__foo__');
-      expect(arguments).to.have.length(1);
+      err.should.be.an.instanceof(Error);
+      err.should.have.property('message');
+      err.message.should.containEql('__foo__');
+      arguments.should.have.length(1);
       done();
     });
   });
@@ -62,30 +51,20 @@ describe('readYaml', function () {
 
 describe('readYaml.sync', function () {
   it('should read the yaml file synchronously.', function () {
-    var expected = testYamlContents;
-    var actual = readYaml.sync(testYamlPath, 'utf8');
-    expect(actual).to.eql(expected);
+    readYaml.sync('test/fixture.yaml', 'utf8').should.eql({foo: {bar: 'baz', qux: true}});
   });
   it('should support options.', function () {
-    var expected = {
-      foo: {
-        bar: 'baz',
-        qux: 'true'
-      }
-    };
-    var actual = readYaml.sync(testYamlPath, {schema: YAML.FAILSAFE_SCHEMA});
-    expect(actual).to.eql(expected);
+    var actual = readYaml.sync('test/fixture.yaml', {schema: YAML.FAILSAFE_SCHEMA});
+    actual.should.eql({foo: {bar: 'baz', qux: 'true'}});
   });
   it('should throw an error when it cannot parse the file as YAML.', function () {
-    var actual = readYaml.sync(testYamlPath);
-    expect(function() {
+    (function() {
       readYaml.sync('README.md', 'utf8');
-    }).to.throw(YAML.YAMLException);
+    }).should.throw(YAML.YAMLException);
   });
   it('should throw an error when it cannot read the file.', function () {
-    var actual = readYaml.sync(testYamlPath);
-    expect(function() {
+    (function() {
       readYaml.sync('node_modules');
-    }).to.throw(/EISDIR/);
+    }).should.throw(/EISDIR/);
   });
 });
